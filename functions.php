288ba92cfed1956e353a7630a7e8da2f6802bb48
Laza <laza@bemapps.com>
2019-10-06 20:25:56 +0200

    make products listed by three in row

--- functions.php
+++ functions.php
@@ -10,18 +10,18 @@ function site_script_enqueue() {
 	wp_enqueue_style('fontawesome', get_template_directory_uri() . '/public/dist/css/font-awesome.min.css', array(), '4.7.0', 'all');
 	wp_enqueue_style('swiper_css', get_template_directory_uri() . '/public/dist/css/swiper.min.css', array(), '3.4.2', 'all');
 	wp_enqueue_style('customstyle', get_template_directory_uri() . '/public/dist/css/app.css', array(), '1.0.0', 'all');
-	
-	
-	
+
+
+
 	//js
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('masonryjs', get_template_directory_uri() . '/public/dist/js/masonry.pkgd.min.js', array(), '1.0.0', true);
 	wp_enqueue_script('bootstrapjs', get_template_directory_uri() . '/public/dist/js/bootstrap.js', array(), '3.3.7', true);
 	wp_enqueue_script('swiperjs', get_template_directory_uri() . '/public/dist/js/swiper.min.js', array(), '3.4.2', true);
 	wp_enqueue_script('globaljs', get_template_directory_uri() . '/public/dist/js/global.js', array(), '1.0.0', true);
-	
-	
-	
+
+
+
 }
 
 add_action( 'wp_enqueue_scripts', 'site_script_enqueue');
@@ -32,12 +32,12 @@ add_action( 'wp_enqueue_scripts', 'site_script_enqueue');
 	==========================================
 */
 function site_theme_setup() {
-	
+
 	add_theme_support('menus');
-	
+
 	register_nav_menu('primary', 'Primary Header Navigation');
 	register_nav_menu('secondary', 'Footer Navigation');
-	
+
 }
 
 add_action('init', 'site_theme_setup');
@@ -68,7 +68,7 @@ function create_posttype() {
         'name' => __( 'Photographers' ),
         'singular_name' => __( 'Photographer' )
       ),
-      
+
       'public' => true,
       'has_archive' => true,
       'menu_icon' => 'dashicons-camera',
@@ -82,7 +82,7 @@ function create_posttype() {
 	  'name' => __( 'Videos' ),
 	  'singular_name' => __( 'Video' )
 	),
-	
+
 	'public' => true,
 	'has_archive' => true,
 	'menu_icon' => 'dashicons-format-video',
@@ -90,7 +90,7 @@ function create_posttype() {
 	'supports' => array('title', 'editor', 'thumbnail', 'author', 'custom-fields')
   )
 );
-  
+
 }
 add_action( 'init', 'create_posttype' );
 
@@ -102,11 +102,11 @@ add_action( 'init', 'create_posttype' );
  * =================================================================================*/
 /*
 function add_to_author_profile( $author_work ) {
-	
-	$contactmethods['work'] = 'Work';	
+
+	$contactmethods['work'] = 'Work';
 	$contactmethods['work_url'] = 'Work website';
-	$contactmethods['facebook'] = 'Facebook profile URL';	
-	$contactmethods['twiter'] = 'Twiter username';	
+	$contactmethods['facebook'] = 'Facebook profile URL';
+	$contactmethods['twiter'] = 'Twiter username';
 	$contactmethods['linkein'] = 'LinkeIn profile URL';
 	return $contactmethods;
 }
@@ -144,9 +144,9 @@ $thumb = new MultiPostThumbnails(array(
 */
 /*
 function site_widget_setup() {
-	
+
 	register_sidebar(
-		array(	
+		array(
 			'name'	=> 'Sidebar',
 			'id'	=> 'sidebar-1',
 			'class'	=> 'custom',
@@ -157,7 +157,7 @@ function site_widget_setup() {
 			'after_title'   => '</h1>',
 		)
 	);
-	
+
 }
 add_action('widgets_init','site_widget_setup');
 */
@@ -187,7 +187,7 @@ function sanitize_pagination($content) {
     $content = preg_replace('#<h2.*?>(.*?)<\/h2>#si', '', $content);
     return $content;
 }
- 
+
 add_action('navigation_markup_template', 'sanitize_pagination');
 
 
@@ -233,4 +233,14 @@ add_action('after_setup_theme', 'woocommerce_support');
 
 function woocommerce_support(){
 	add_theme_support('woocommerce');
+}
+
+/**
+ * Change number or products per row to 3
+ */
+add_filter('loop_shop_columns', 'loop_columns', 999);
+if (!function_exists('loop_columns')) {
+    function loop_columns() {
+        return 3; // 3 products per row
+    }
 }
